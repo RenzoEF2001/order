1cd14a5f5ef5418e1fceffde96d9ecb77cec590c
RenzoEF2001 <i76240662@isise.edu.pe>
2022-01-26 17:22:31 -0500

    model

--- application/models/DispositivoModel.php
+++ application/models/DispositivoModel.php
@@ -20,7 +20,17 @@ class DispositivoModel extends CI_Model
         $this->db->from('tb_dispositivo');
         $this->db->join('tb_tipo_sistema', 'tb_tipo_sistema.ID_TIPOSISTEMA = tb_dispositivo.FK_TIPOSISTEMA', 'inner');
         $this->db->where('tb_dispositivo.ESTADO', 1);
+        $this->db->where('tb_tipo_sistema.COD_TIPOSISTEMA', $codigo);
         return  $this->db->get()->result_array();
+    }
+
+    public function getIdPerCodigo($codigo)
+    {
+        $this->db->select('ID_DISPOSITIVO');
+        $this->db->from('tb_dispositivo');
+        $this->db->where('tb_dispositivo.ESTADO', 1);
+        $this->db->where('tb_dispositivo.COD_DISPOSITIVO', $codigo);
+        return  $this->db->get()->row_array()['ID_DISPOSITIVO'];
     }
 
 }
