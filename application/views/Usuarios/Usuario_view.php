24a61235e110d514c4af3778bf207a934de1c11d
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-05 11:17:44 -0500

    view

--- application/views/Usuarios/Usuario_view.php
+++ application/views/Usuarios/Usuario_view.php
@@ -5,86 +5,38 @@
                 <center>
                     <h2>Administración de usuarios</h2>
                 </center>
-                <form action="">
-                    <div class="dvbusq">
-                        <input type="text" name="" id="" class="form-control" placeholder="Buscar...">
-
-                        <button type="submit" class="btn btn-inverse-primary flt" hidden>Filtrar</button>
-                    </div>
-                </form>
                 <div class="card tablescroll">
-                    <table class="table table-hover">
+                    <table class="table table-hover" id="idTablaUsuario">
                         <thead>
                             <tr>
-                        <b>
-                            <th scope="col" class="cbz">#</th>
-                        </b>
-                        <th scope="col" class="cbz">Editar</th>
-                        <th scope="col" class="cbz">Eliminar</th>
-                        <th scope="col" class="cbz">First</th>
-                        <th scope="col" class="cbz">Last</th>
-                        <th scope="col" class="cbz">Handle</th>
-                        <th scope="col" class="text-center">Foto</th>
-                        </tr>
+                                <b>
+                                    <th scope="col" class="cbz">#</th>
+                                </b>
+                                <th scope="col" class="text-center">COD USUARIO</th>
+                                <th scope="col" class="text-center">USUARIO</th>
+                                <th scope="col" class="text-center">PERFIL</th>
+                                <th scope="col" class="text-center">EMPLEADO</th>
+                                <th scope="col" class="text-center">FOTO</th>
+                                <th scope="col" class="text-center">Editar</th>
+                                <th scope="col" class="text-center">Eliminar</th>
+                            </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td><a href="<?php echo site_url('usuario/edit'); ?>"><i class="fas fa-edit"></i></a></td>
-                                <td><a href=""><i class="fas fa-trash"></i></a></td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td class="text-center"><img data-toggle="tooltip" data-placement="right" title="<img class='w-100' src='<?php echo base_url(); ?>assets/images/evidencias/evi3.jpg' />" src="../../assets/images/evidencias/evi3.jpg"></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td><a href="<?php echo site_url('usuario/edit'); ?>"><i class="fas fa-edit"></i></a></td>
-                                <td><a href=""><i class="fas fa-trash"></i></a></td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td class="text-center"><img data-toggle="tooltip" data-placement="right" title="<img class='w-100' src='<?php echo base_url(); ?>assets/images/evidencias/evi3.jpg' />" src="../../assets/images/evidencias/evi3.jpg"></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td><a href="<?php echo site_url('usuario/edit'); ?>"><i class="fas fa-edit"></i></a></td>
-                                <td><a href=""><i class="fas fa-trash"></i></a></td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td class="text-center"><img data-toggle="tooltip" data-placement="right" title="<img class='w-100' src='../../assets/images/evidencias/evi3.jpg' />" src="../../assets/images/evidencias/evi3.jpg"></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td><a href="<?php echo site_url('usuario/edit'); ?>"><i class="fas fa-edit"></i></a></td>
-                                <td><a href=""><i class="fas fa-trash"></i></a></td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td class="text-center"><img data-toggle="tooltip" data-placement="right" title="<img class='w-100' src='../../assets/images/evidencias/evi3.jpg' />" src="../../assets/images/evidencias/evi3.jpg"></td>
-                            </tr>
+                            <?php $count = 0; ?>
+                            <?php foreach($Usuario as $valor): ?>
+                                <tr>
+                                    <td><?= ++$count ?></td>
+                                    <td><?= $valor->COD_USUARIO ?></td>
+                                    <td><?= $valor->USUARIO ?></td>
+                                    <td><?= $valor->NOMBRE ?></td>
+                                    <td><?= $valor->NOMBRES ?></td>
+                                    <td class="text-center" ><img data-toggle="tooltip" title="<img class='w-100' src='<?= base_url() ?>assets/images/usuarios/<?= $valor->FOTO ?>'/>" data-placement="right" src="<?= base_url() ?>assets/images/usuarios/<?= $valor->FOTO ?>"/></td>
+                                    <td class="text-center"><a href="<?= site_url('Usuario/llenarCampos_Usuario/'.$valor->ID_USUARIO) ?>"><i class="fas fa-edit"></i></a></td>
+                                    <td class="text-center"><a class="btnEliminarEmpleado" href="<?= site_url('Usuario/eliminar_Usuario/'.$valor->ID_USUARIO) ?>" data-codigo="<?= $valor->COD_USUARIO ?>"><i class="fas fa-trash text-danger"></i></a></td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
-                </div>
-                <div class="centerpagination">
-                    <div class="itmpagiationcenter">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div>
-                        <button type="button" class="btn btn-greenexcel"> <i class="mdi mdi-file-excel"></i>
-                            Exportar Excel</button>
-                        <button type="button" class="btn btn-dark" onClick='window.history.back()'> <i
-                                class="mdi mdi-keyboard-return"></i> Atras</button>
-                    </div>
                 </div>
             </div>
         </div>
