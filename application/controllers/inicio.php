a14a85bf499a9bce84cfc3bd224255c205a1e838
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-05 10:54:24 -0500

    controller

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -6,11 +6,24 @@ class Inicio extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model("OrdenModel");
     }
 
     public function index() {
+        date_default_timezone_set('America/Lima');
+        $creadas = $this->OrdenModel->getCantidadOrdenes_Mes(1, date('n', time()));
+        $pendiente = $this->OrdenModel->getCantidadOrdenes_Mes(2, date('n', time()));
+        $trabajando = $this->OrdenModel->getCantidadOrdenes_Mes(3, date('n', time()));
+
+        $data = [
+            "creadas" => $creadas,
+            "pendiente" => $pendiente,
+            "trabajando" => $trabajando
+        ];
+
         $this->load->view('cabecera');
-        $this->load->view('inicio_v');
+        $this->load->view('inicio_v', $data);
         $this->load->view('footer');
     }
 }
