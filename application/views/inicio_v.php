ab8c8bc06b6c5fe8c0f28e0a0343b36e3f4ef36b
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-05 11:22:01 -0500

    view

--- application/views/inicio_v.php
+++ application/views/inicio_v.php
@@ -1,52 +1,80 @@
-<!-- partial -->
 <div class="main-panel">
     <div class="content-wrapper">
-        <div class="row" id="proBanner">
-            <div class="col-12">
-                <span class="d-flex align-items-center purchase-popup">
-                    <p>Notificaciones pendientes!</p>
-                    <a href="view/Orden/Orden_view.html"
-                       class="btn download-button purchase-button ml-auto">Revisar</a>
-                    <i class="mdi mdi-close" id="bannerClose"></i>
-                </span>
-            </div>
-        </div>
-        <div class="page-header">
-            <h3 class="page-title">
-                <span class="page-title-icon bg-gradient-primary text-white mr-2">
-                    <i class="mdi mdi-home"></i>
-                </span> Dashboard
-            </h3>
-            <nav aria-label="breadcrumb">
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page">
-                        <span></span> <i
-                            class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <h1>REPORTE DE ORDENES TÉCNICAS</h1>
         <div class="row">
-            <div class="col-md-7 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="clearfix">
-                            <h4 class="card-title float-left">Datos Estadisticos</h4>
-                            <div id="visit-sale-chart-legend"
-                                 class="rounded-legend legend-horizontal legend-top-right float-right"></div>
+            <div class="col-12">
+                <h1 class="p-3 ml-1">REPORTE DE ORDENES TÉCNICAS</h1>
+                <div class="row mt-3">
+                    <div id="divCreadaReporte" class="col-md-4 stretch-card grid-margin cardButton">
+                        <div class="card bg-gradient-danger card-img-holder text-white">
+                            <div class="card-body">
+                                <img src="assets/images/dashboard/circle.svg" class="card-img-absolute"
+                                    alt="circle-image" />
+                                <h4 class="font-weight-normal mb-3">Ordenes Creadas <i
+                                        class="mdi mdi-plus-circle mdi-24px float-right"></i>
+                                </h4>
+                                <h2 class="mb-5"><?= $creada[0]['CANTIDAD'] ?></h2>
+                                <h6 class="card-text"><?= date('F') ?></h6>
+                            </div>
                         </div>
-                        <canvas id="visit-sale-chart" class="mt-4"></canvas>
+                    </div>
+                    <div id="divPendienteReporte" class="col-md-4 stretch-card grid-margin cardButton">
+                        <div class="card rgbPendiente card-img-holder text-white">
+                            <div class="card-body">
+                                <img src="assets/images/dashboard/circle.svg" class="card-img-absolute"
+                                    alt="circle-image" />
+                                <h4 class="font-weight-normal mb-3">Ordenes Pendientes<i
+                                        class=" mdi mdi-timer-sand  mdi-24px float-right"></i>
+                                </h4>
+                                <h2 class="mb-5"><?= $pendiente[0]['CANTIDAD'] ?></h2>
+                                <h6 class="card-text"><?= date('F') ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="divProcesoReporte" class="col-md-4 stretch-card grid-margin cardButton">
+                        <div class="card rgbTrabajando card-img-holder text-white">
+                            <div class="card-body">
+                                <img src="assets/images/dashboard/circle.svg" class="card-img-absolute"
+                                    alt="circle-image" />
+                                <h4 class="font-weight-normal mb-3">Ordenes en Proceso<i
+                                        class="mdi mdi-sync mdi-24px float-right"></i>
+                                </h4>
+                                <h2 class="mb-5"><?= $trabajando[0]['CANTIDAD'] ?></h2>
+                                <h6 class="card-text"><?= date('F') ?></h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-5 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Traffic Sources</h4>
-                        <canvas id="traffic-chart"></canvas>
-                        <div id="traffic-chart-legend"
-                             class="rounded-legend legend-vertical legend-bottom-left pt-4"></div>
+                <div class="row">
+                    <div class="col-md-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="clearfix">
+                                    <h4 class="card-title float-left">Ordenes pendientes y cerradas por mes </h4>
+                                    <div id="visit-sale-chart-legend"
+                                        class="rounded-legend legend-horizontal legend-top-right float-right"></div>
+                                </div>
+                                <canvas id="visit-sale-chart" class="mt-4"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Ordenes por atender</h4>
+                                <h6 class="card-subtitle"><?= date('F') ?></h6>
+                                <canvas id="doughnutChart" style="height:250px"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Tipos de sistemas mas solicitados</h4>
+                                <canvas id="areaChart" style="height:250px"></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -65,4 +93,3 @@
     <!-- partial -->
 </div>
 <!-- main-panel ends -->
-
