0bda91f922da0c2fd78fa847de8f0cd7504c255b
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-05 11:00:52 -0500

    model

--- application/models/EmpleadoModel.php
+++ application/models/EmpleadoModel.php
@@ -11,14 +11,25 @@ class EmpleadoModel extends CI_Model
     public function create($datos)
     {
         $query = $this->db->insert('tb_empleado',$datos);
+        $idCreada = $this->db->insert_id();
+
+        $this->db->set('COD_EMPLEADO', $this->callSpGenerateCode('EMP-',$idCreada)['CODIGO']);
+        $this->db->where('ID_EMPLEADO', $idCreada);
+        $this->db->update('tb_empleado'); 
+
+        $data = [
+            "status" => $query,
+            "id" => $idCreada
+        ];
+
         mysqli_next_result( $this->db->conn_id );
-        return $query;
+        return $data;
     }
 
     /* Usado para registrar el usuario, el estado por defecto es 1 */
     public function getLast()
     {
-        return $res = $this->db->get('tb_empleado')->last_row('array');
+        return $this->db->get('tb_empleado')->last_row('array');
     }
 
     public function get()
@@ -26,6 +37,22 @@ class EmpleadoModel extends CI_Model
         $this->db->from('tb_empleado');
         $this->db->where('ESTADO', 1);
         return  $this->db->get()->result_array();
+    }
+
+    public function getTecnicos()
+    {
+        $this->db->from('tb_empleado');
+        $this->db->join('tb_usuario', 'tb_usuario.FK_EMPLEADO = tb_empleado.ID_EMPLEADO', 'inner');
+        $this->db->where('tb_empleado.ESTADO', 1);
+        $this->db->where('tb_usuario.FK_PERFIL', 1);
+        return  $this->db->get()->result_array();
+    }
+
+    public function getById($id)
+    {
+        $this->db->from('tb_empleado');
+        $this->db->where('ID_EMPLEADO', $id);
+        return $this->db->get()->row_array();
     }
 
     /* Usado para rellenar campos del formulario actualizar, no es necesario validar si su estado es 1  */
@@ -50,11 +77,8 @@ class EmpleadoModel extends CI_Model
         return $status;
     }
 
-    public function callSpGenerateCode($nomenclatura)
+    public function callSpGenerateCode($nomenclatura, $num)
     {
-        $num = $this->getLast()['ID_EMPLEADO'];
-        ++$num;
-
         $query = $this->db->query("CALL `sp_generar_codigo`('$nomenclatura','$num')");
         mysqli_next_result( $this->db->conn_id );
         return $query->row_array();
