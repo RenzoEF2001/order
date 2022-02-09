0160950b71840251eb3ce8a4d266ea65a256e772
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-08 23:36:55 -0500

    model

--- application/models/OrdenDetalleModel.php
+++ application/models/OrdenDetalleModel.php
@@ -36,6 +36,13 @@ class OrdenDetalleModel extends CI_Model
         $this -> db -> update ('tb_orden_detalle'); 
     }
 
+    public function updateImagenEvidencia($imagen, $codigo)
+    {
+        $this -> db -> set ('IMAGENES_EVIDENCIA' , $imagen); 
+        $this -> db -> where ('COD_ORDEN_DETALLE' , $codigo); 
+        $this -> db -> update ('tb_orden_detalle'); 
+    }
+
     public function getPerOrden($codigo)
     {
         $this->db->select('tb_tipo_sistema.DESCRIPCION, tb_dispositivo.NOMBRE,  tb_orden_detalle.DESCRIPCION AS DESCRIPCION_PROBLEMA, tb_orden_detalle.COD_ORDEN_DETALLE');
@@ -49,7 +56,7 @@ class OrdenDetalleModel extends CI_Model
 
     public function getPerCode($codigo)
     {
-        $this->db->select('tb_tipo_sistema.DESCRIPCION, tb_dispositivo.NOMBRE,  tb_orden_detalle.DESCRIPCION AS DESCRIPCION_PROBLEMA, tb_orden_detalle.COD_ORDEN_DETALLE, tb_orden_detalle.IMAGENES');
+        $this->db->select('tb_tipo_sistema.DESCRIPCION, tb_dispositivo.NOMBRE,  tb_orden_detalle.DESCRIPCION AS DESCRIPCION_PROBLEMA, tb_orden_detalle.COD_ORDEN_DETALLE, tb_orden_detalle.IMAGENES, tb_orden_detalle.IMAGENES_EVIDENCIA');
         $this->db->from('tb_orden_detalle');
         $this->db->join('tb_orden', 'tb_orden.ID_ORDEN = tb_orden_detalle.FK_ORDEN', 'inner');
         $this->db->join('tb_dispositivo', 'tb_dispositivo.ID_DISPOSITIVO = tb_orden_detalle.FK_DISPOSITIVO', 'inner');
