4a8fbb5ddd7ea9515c0601343dd82f3489de4988
RenzoEF2001 <i76240662@isise.edu.pe>
2022-02-08 23:41:03 -0500

    view

--- application/views/Orden/Orden_atendida.php
+++ application/views/Orden/Orden_atendida.php
@@ -19,36 +19,44 @@
                                         <th scope="col" class="cbz">Sucursal</th>
                                         <th scope="col" class="text-center">Ver Detalles</th>
                                         <th scope="col" class="text-center">Estado</th>
+                                        <th scope="col" class="text-center"> </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php foreach($ordenesatendidas as $valor): ?>
-                                        <tr>
-                                            <td><?= $valor['COD_ORDEN'] ?></td>
-                                            <td><?= $valor['FECHA_ORDEN'] ?></td>
-                                            <td><?= $valor['HORA_ORDEN'] ?></td>
-                                            <td
-                                                style="max-width: 200px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">
-                                                <?= $valor['ASUNTO'] ?></td>
-                                            <td><?= $valor['RAZON_SOCIAL'] ?></td>
-                                            <td
-                                                style="max-width: 100px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">
-                                                <?= $valor['DIRECCION_SUCURSAL'] ?></td>
-                                            <td class="text-center"><button type="button"
-                                                    class="btnDetallesOrdenAtendida btn btn-outline btn-rounded btn-icon"
-                                                    data-codigo="<?= $valor['COD_ORDEN'] ?>"><i
-                                                        class="mdi mdi-library-books mdi-18px text-info"></i></button></td>
-                                            <?php if($valor['ESTADO_ORDEN'] == 4): ?>
-                                                <td class="text-center"><button data-codigo="<?= $valor['COD_ORDEN'] ?>"
-                                                        class="btnAtendidaOrdenAtendida text-black-50 font-weight-bold badge badge-info">ATENDIDA</button>
-                                                </td>
-                                            <?php endif; ?>
-                                            <?php if($valor['ESTADO_ORDEN'] == 5): ?>
-                                                <td class="text-center"><label
-                                                        class="text-black-50 font-weight-bold badge badge-secondary">CERRADA</label>
-                                                </td>
-                                            <?php endif; ?>
-                                        </tr>
+                                    <tr>
+                                        <td><?= $valor['COD_ORDEN'] ?></td>
+                                        <td><?= $valor['FECHA_ORDEN'] ?></td>
+                                        <td><?= $valor['HORA_ORDEN'] ?></td>
+                                        <td
+                                            style="max-width: 200px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">
+                                            <?= $valor['ASUNTO'] ?></td>
+                                        <td><?= $valor['RAZON_SOCIAL'] ?></td>
+                                        <td
+                                            style="max-width: 100px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">
+                                            <?= $valor['DIRECCION_SUCURSAL'] ?></td>
+
+                                        <td class="text-center"><button type="button"
+                                                class="btnDetallesOrdenAtendida btn btn-outline btn-rounded btn-icon"
+                                                data-codigo="<?= $valor['COD_ORDEN'] ?>"><i
+                                                    class="mdi mdi-library-books mdi-18px text-info"></i></button></td>
+                                        <?php if($valor['ESTADO_ORDEN'] == 4): ?>
+                                        <td class="text-center"><button data-codigo="<?= $valor['COD_ORDEN'] ?>"
+                                                class="btnAtendidaOrdenAtendida text-black-50 font-weight-bold badge badge-info">ATENDIDA</button>
+                                        </td>
+                                        <td class="text-center"><a type="button"
+                                                class="btnAgregarEvidenciaOrdenAtendida btn btn-outline btn-rounded btn-icon d-flex align-items-center justify-content-center"
+                                                data-codigo="<?= $valor['COD_ORDEN'] ?>" data-toggle="tooltip" data-placement="top"
+                                                title="Agregar Evidencia"><i
+                                                    class="mdi mdi-image-multiple mdi-18px text-primary"></i></a></td>
+                                        <?php endif; ?>
+                                        <?php if($valor['ESTADO_ORDEN'] == 5): ?>
+                                        <td class="text-center"><label
+                                                class="text-black-50 font-weight-bold badge badge-secondary">CERRADA</label>
+                                        </td>
+                                        <td></td>
+                                        <?php endif; ?>
+                                    </tr>
                                     <?php endforeach; ?>
                                 </tbody>
                             </table>
@@ -215,6 +223,81 @@
                     class="btn btn-gradient-dark btn-icon-text d-flex align-items-center"><i
                         class="mdi mdi-arrow-left  btn-icon-prepend"></i>Atras
                 </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalAgregarEvidenciaOrdenAtendida" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="row-fluid">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Detalle de orden</h4>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group row">
+                                            
+                                            <div class="col-sm-12">
+                                                <select id="cboDetallesOrdenesOrdenAtendida" class="form-control">
+                                                    <option value="-1">Primero seleccione una orden</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                                <h4 class="card-title">Agregar Evidencia</h4>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group row">
+                                            <div class="col-sm-9">
+                                                <input id="inputAgregarEvidenciaOrdenAtendida" class="form-control" type="file" accept=".jpg,.jpeg,.png" multiple>
+                                            </div>
+                                            <button id="btnAgregarEvidenciaOrdenAtendida" class="btn btn-primary col-sm-3" disabled>Agregar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                                <h4 class="card-title">Evidencias Actuales</h4>
+                                <div class="row">
+                                    <div class="col col-md-12">
+                                        <div class="form-group">
+                                            <div id="carouselEvidenciaControls" class="carousel slide"
+                                                data-ride="carousel">
+                                                <div id="carruselEvidenciaOrdenAtendida" class="carousel-inner">
+                                                    <div class="carousel-item active">
+                                                        <img class="d-block w-100"
+                                                            src="<?= base_url() ?>assets/images/ordenes/no_disponible.png"
+                                                            alt="First slide">
+                                                    </div>
+                                                </div>
+                                                <a class="carousel-control-prev" href="#carouselEvidenciaControls"
+                                                    role="button" data-slide="prev">
+                                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                    <span class="sr-only">Previous</span>
+                                                </a>
+                                                <a class="carousel-control-next" href="#carouselEvidenciaControls"
+                                                    role="button" data-slide="next">
+                                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                    <span class="sr-only">Next</span>
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
         </div>
     </div>
